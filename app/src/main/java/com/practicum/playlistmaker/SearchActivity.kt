4bc5e5afa8026ecc631fb828698c240a8633d949
i-Mondzy <moonway155@gmail.com>
2025-01-11 20:17:58 +0600

    Search screen 2

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,9 +1,11 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
@@ -33,6 +35,11 @@ class SearchActivity : AppCompatActivity() {
 
         clearButton.setOnClickListener{
             inputEditText.setText("")
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+            val currentView = currentFocus ?: window.decorView
+
+            inputMethodManager?.hideSoftInputFromWindow(currentView.windowToken, 0)
+            currentView.clearFocus()
         }
 
         val simpleTextWatcher = object : TextWatcher {
