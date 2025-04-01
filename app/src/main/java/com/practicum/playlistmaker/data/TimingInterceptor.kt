b33409c6752f21fe6c5c91f2c8b3a725e58a3bac
Victor <moonway155@gmail.com>
2025-04-02 02:01:34 +0600

    Track List - add Debounce + MediaPlayer (review ready)

---
+++ app/src/main/java/com/practicum/playlistmaker/data/TimingInterceptor.kt
@@ -0,0 +1,19 @@
+package com.practicum.playlistmaker.data
+
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class TimingInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val startNs = System.nanoTime()
+
+        val response = chain.proceed(request)
+
+        val durationMs = (System.nanoTime() - startNs) / 1_000_000
+        Log.d("Retrofit", "Запрос ${request.url()} занял: $durationMs мс")
+
+        return response
+    }
+}
--- app/src/main/java/com/practicum/playlistmaker/data/recycler/Track.kt
+++ app/src/main/java/com/practicum/playlistmaker/data/recycler/Track.kt
@@ -13,5 +13,6 @@ data class Track(
     val collectionName: String, //Название альбома
     val releaseDate: String, // Год релиза трека
     val primaryGenreName: String, // Жанр трека
-    val country: String // Страна исполнителя
+    val country: String, // Страна исполнителя
+    val previewUrl: String // Отрывок трека
 ) : Parcelable
