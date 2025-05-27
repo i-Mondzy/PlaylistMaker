420059cb9d1924e55f552e730b1a7b9b7703a433
Victor <moonway155@gmail.com>
2025-05-28 00:47:14 +0600

    экран Медиатека готов к проверке спринт 18

--- app/src/main/java/com/practicum/playlistmaker/media/ui/fragment/FavoriteFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/media/ui/fragment/FavoriteFragment.kt
@@ -5,17 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.practicum.playlistmaker.R
+import com.practicum.playlistmaker.databinding.FragmentFavoriteBinding
+import com.practicum.playlistmaker.media.ui.view_model.FavoriteViewModule
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoriteFragment : Fragment() {
 
-    companion object {  }
+    private val viewModel by viewModel<FavoriteViewModule>()
+
+    private var binding: FragmentFavoriteBinding? = null
+
+    companion object {
+        fun newInstance() = FavoriteFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        return binding?.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
